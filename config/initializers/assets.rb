8b130f10478020677acea9d54501ba6ed36baafa
Fernanda <fer.dantas07@gmail.com>
2025-01-23 16:30:20 -0300

    ajustando as telas de admin e user

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,10 +12,10 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # app/assets
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css users_backoffice.js users_backoffice.css )
+Rails.application.config.assets.precompile += %w( application.js application.css admins_backoffice.js admins_backoffice.css users_backoffice.js users_backoffice.css )
 
 # lib/assets
-Rails.application.config.assets.precompile += %w( custom.js custom.css img.jpg )
+Rails.application.config.assets.precompile += %w( custom.js custom.css img.jpg _img.png )
 
 # vendor/assets
 Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery )
