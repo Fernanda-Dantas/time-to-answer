e4c8affa7a6aeb4277b31482c621baa79b249ef3
Fernanda Dantas <fer.dantas07@gmail.com>
2025-12-02 07:58:40 -0300

    add nova coluna de contador de questoes por assunto no BD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_29_132653) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_02_104336) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_29_132653) do
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "questions_count"
   end
 
   create_table "users", force: :cascade do |t|
